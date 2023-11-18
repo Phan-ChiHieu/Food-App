ea20875cc2dd2ae8583812282354507123a9db43
EdricPhan <edricphan.dev@gmail.com>
2023-11-18 10:27:21 +0700

    update code

---
+++ app/_layout.tsx
@@ -0,0 +1,25 @@
+import CustomHeader from "@/components/CustomHeader";
+import { Stack, useNavigation } from "expo-router";
+
+
+export const unstable_settings = {
+    // Ensure that reloading on `/modal` keeps a back button present.
+    initialRouteName: 'index',
+  };
+
+
+export default function RootLayoutNav() {
+  const navigation = useNavigation();
+
+  //
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          header: () => <CustomHeader />,
+        }}
+      />
+    </Stack>
+  );
+}
