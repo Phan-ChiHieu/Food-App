ae78ab3371c1645bd67d744acca8260cfe3fe8f8
EdricPhan <edricphan.dev@gmail.com>
2023-11-21 11:36:07 +0700

    update code

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,37 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, StyleSheet } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Categories from "@/components/Categories";
+import Restaurants from "@/components/Restaurants";
 const Page = () => {
   return (
-    <SafeAreaView>
-      <Text>Page</Text>
+    <SafeAreaView style={styles.container}>
+      <ScrollView
+        contentContainerStyle={{
+          paddingBottom: 40,
+        }}
+      >
+        <Categories />
+        <Text style={styles.header}>Top picks in your neighbourhood</Text>
+        <Restaurants />
+        <Text style={styles.header}>Offers near you</Text>
+        <Restaurants />
+      </ScrollView>
     </SafeAreaView>
   );
 };
 
 export default Page;
+
+const styles = StyleSheet.create({
+  container: {
+    top: 50,
+    backgroundColor: "#fff",
+  },
+  header: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 16,
+    paddingHorizontal: 16,
+  },
+});
