7ca1b9431e96f343e1d39b8d03387cc9eb6d527b
EdricPhan <edricphan.dev@gmail.com>
2023-11-22 11:26:36 +0700

    update code

--- app/details.tsx
+++ app/details.tsx
@@ -1,16 +1,59 @@
-import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
-import React, { useLayoutEffect } from "react";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  SectionList,
+  ListRenderItem,
+  ScrollView,
+} from "react-native";
+import React, { useLayoutEffect, useRef, useState } from "react";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { restaurant } from "@/assets/data/restaurant";
 import Colors from "@/themes/Color";
-import { useNavigation } from "expo-router";
+import { Link, useNavigation } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
+import Animated, { useAnimatedStyle, useSharedValue, withTiming } from "react-native-reanimated";
 
 const DetailsPage = () => {
   const navigation = useNavigation();
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const scrollRef = useRef<ScrollView>(null);
+  const itemsRef = useRef<TouchableOpacity[]>([]);
+
+  const opacity = useSharedValue(0);
+  const animatedStyles = useAnimatedStyle(() => ({
+    opacity: opacity.value,
+  }));
+
+  const DATA = restaurant.food.map((item, index) => ({
+    title: item.category,
+    data: item.meals,
+    index,
+  }));
+
+  const selectCategory = (index: number) => {
+    const selected = itemsRef.current[index];
+    setActiveIndex(index);
+
+    selected.measure((x) => {
+      scrollRef.current?.scrollTo({ x: x - 16, y: 0, animated: true });
+    });
+  };
+
+  const onScroll = (event: any) => {
+    const y = event.nativeEvent.contentOffset.y;
+
+    if (y > 350) {
+      opacity.value = withTiming(1);
+    } else {
+      opacity.value = withTiming(0);
+    }
+  };
 
   useLayoutEffect(() => {
-    console.log("Details");
     navigation.setOptions({
       headerTransparent: true,
       headerTitle: "",
@@ -33,9 +76,23 @@ const DetailsPage = () => {
     });
   }, []);
 
+  const renderItem: ListRenderItem<any> = ({ item, index }) => (
+    <Link href={{ pathname: "/(modal)/dish", params: { id: item.id } }} asChild>
+      <TouchableOpacity style={styles.item}>
+        <View style={{ flex: 1 }}>
+          <Text style={styles.dish}>{item.name}</Text>
+          <Text style={styles.dishText}>{item.info}</Text>
+          <Text style={styles.dishText}>${item.price}</Text>
+        </View>
+        <Image source={item.img} style={styles.dishImage} />
+      </TouchableOpacity>
+    </Link>
+  );
+
   return (
     <>
       <ParallaxScrollView
+        scrollEvent={onScroll}
         backgroundColor={"#fff"}
         style={{ flex: 1 }}
         parallaxHeaderHeight={250}
@@ -50,10 +107,59 @@ const DetailsPage = () => {
           </View>
         )}
       >
-        <View style={{ height: 500 }}>
-          <Text>DetailsPage</Text>
+        <View style={styles.detailsContainer}>
+          <Text style={styles.restaurantName}>{restaurant.name}</Text>
+          <Text style={styles.restaurantDescription}>
+            {restaurant.delivery} ·{" "}
+            {restaurant.tags.map(
+              (tag, index) => `${tag}${index < restaurant.tags.length - 1 ? " · " : ""}`
+            )}
+          </Text>
+          <Text style={styles.restaurantDescription}>{restaurant.about}</Text>
+          <SectionList
+            contentContainerStyle={{ paddingBottom: 50 }}
+            keyExtractor={(item, index) => `${item.id + index}`}
+            scrollEnabled={false}
+            sections={DATA}
+            renderItem={renderItem}
+            ItemSeparatorComponent={() => (
+              // Đường ngăn cách giữa các phần tử trong 1 list nhỏ
+              <View style={{ marginHorizontal: 16, height: 1, backgroundColor: Colors.grey }} />
+            )}
+            SectionSeparatorComponent={() => (
+              // Đường ngăn cách giữa các phần tử trong 1 list lơn hơn
+              <View style={{ height: 1, backgroundColor: Colors.grey }} />
+            )}
+            renderSectionHeader={({ section: { title, index } }) => (
+              <Text style={styles.sectionHeader}>{title}</Text>
+            )}
+          />
         </View>
       </ParallaxScrollView>
+
+      <Animated.View style={[styles.stickySegments, animatedStyles]}>
+        <View style={styles.segmentsShadow}>
+          <ScrollView
+            ref={scrollRef}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={styles.segmentScrollview}
+          >
+            {restaurant.food.map((item, index) => (
+              <TouchableOpacity
+                ref={(ref) => (itemsRef.current[index] = ref!)}
+                key={index}
+                style={activeIndex === index ? styles.segmentButtonActive : styles.segmentButton}
+                onPress={() => selectCategory(index)}
+              >
+                <Text style={activeIndex === index ? styles.segmentTextActive : styles.segmentText}>
+                  {item.category}
+                </Text>
+              </TouchableOpacity>
+            ))}
+          </ScrollView>
+        </View>
+      </Animated.View>
     </>
   );
 };
@@ -215,6 +321,5 @@ const styles = StyleSheet.create({
     fontSize: 16,
   },
 });
-
 
 // 2:58
